1c4998214f4713079051c7ec019eff1ff720bfab
LavoussierDev <Onilr31@gmail.com>
2023-10-15 23:54:18 -0400

    Updated files

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,7 +7,10 @@
  */
 int main(void)
 {
+	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(float));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(double));
 
 	return (0);
 }
-
