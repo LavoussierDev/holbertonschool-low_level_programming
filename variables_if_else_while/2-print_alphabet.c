a2b542d48e75b8d369527d517f2c0ed0534ba8b9
LavoussierDev <Onilr31@gmail.com>
2023-10-18 19:42:39 -0400

    first commit

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints out lower case
+ * alphabet.
+ *
+ * Return: Always 0
+ */
+
+int main()
+{
+	int ch;
+	for(ch = 'a'; <= 'z'; ch++)
+		putchar(ch);
+	return (0);
+}
