cc0e88db1e5d27fc82f95b9b949a159aa911a088
LavoussierDev <Onilr31@gmail.com>
2023-10-19 13:47:05 -0400

    first commit

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits 0-10
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x;
+	
+	for(x = 0; x<=9; x++)
+	{
+		printf("%d", x);
+	}
+		printf("\n");
+
+	return (0);
+}
