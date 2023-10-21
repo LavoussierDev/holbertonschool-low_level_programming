1c9e9529b3b20ca7e49615fc6af761ae079a0567
LavoussierDev <Onilr31@gmail.com>
2023-10-21 10:17:07 -0400

    first commit

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints out to stdout
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	
+	return (0);
+}
