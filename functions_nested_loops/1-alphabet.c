d35fb09fe85a840a2926009f739ed523298f65d9
LavoussierDev <Onilr31@gmail.com>
2023-10-22 19:51:20 -0400

    first commit

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int x = 'a';
+
+	while (x <= 'z')
+	{
+		_putchar(x);
+		x++;
+	}
+		_putchar('\n');
+
+		return;
+}
+
+
+
