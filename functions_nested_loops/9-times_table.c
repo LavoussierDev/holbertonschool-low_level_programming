70553eca84b1520bbca2e113aa21ff2a6976e592
LavoussierDev <Onilr31@gmail.com>
2023-10-23 15:27:32 -0400

    backup x2

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,16 +15,16 @@ void times_table(void)
 		for (y = 0; y < 10; y++)
 		{
 			mul = x * y;
-			_putchar((mul * 0) + '0');
-			_putchar((mul * 1) + '0');
-			_putchar((mul * 2) + '0');
-			_putchar((mul * 3) + '0');
-			_putchar((mul * 4) + '0');
-			_putchar((mul * 5) + '0');
-			_putchar((mul * 6) + '0');
-			_putchar((mul * 7) + '0');
-			_putchar((mul * 8) + '0');
-			_putchar((mul * 9) + '0');
+			_putchar(mul + '0');
+			_putchar(mul + '0');
+			_putchar(mul + '0');
+			_putchar(mul + '0');
+			_putchar(mul + '0');
+			_putchar(mul + '0');
+			_putchar(mul + '0');
+			_putchar(mul + '0');
+			_putchar(mul + '0');
+			_putchar(mul + '0');
 		}
 	}
 }
