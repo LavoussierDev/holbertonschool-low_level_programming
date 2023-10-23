b883b78df2f3e32ac806d55091e151bf49e7fe1c
LavoussierDev <Onilr31@gmail.com>
2023-10-22 23:44:49 -0400

    first commit

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value
+ * of an integer
+ *
+ * Return: Results
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
+
