8db467b7f995840075229ba8fae9fc114ba86355
LavoussierDev <Onilr31@gmail.com>
2023-10-24 14:30:49 -0400

    fix: Standard syntax format

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -3,12 +3,17 @@
 /**
  * add - adds two intengers
  *
+ * @x: input of given number
+ * @y: input of given number
+ *
  * Return: Results
  */
 
 int add(int x, int y)
 {
 	int r;
+
 	r = x + y;
-	return(r);
+
+	return (r);
 }
