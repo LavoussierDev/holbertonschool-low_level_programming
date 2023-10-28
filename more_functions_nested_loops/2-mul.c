7a71a7491b3c53f8408850402a41e75cf45fe2b8
LavoussierDev <Onilr31@gmail.com>
2023-10-28 14:57:40 -0400

    fix: organized syntax format

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -2,13 +2,14 @@
 
 /**
  * mul - multiplies two integers
- * @a: first digit @b: second digit
+ * @a: first digit
+ * @b: second digit
  *
  * Return: the results
  */
 
 int mul(int a, int b)
 {
-	return a * b;
+	return (a * b);
 	return (0);
 }
