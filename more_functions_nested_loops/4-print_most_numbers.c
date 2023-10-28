81e4e4a537f7981785dda9dbe372abbb32579d3a
LavoussierDev <Onilr31@gmail.com>
2023-10-28 15:51:50 -0400

    first commit

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints digits 0 - 9,
+ * except 2 & 4.
+ *
+ * Return: results
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+		for (x = '0'; x < '9'; x++)
+		{
+			if (x != '2' && x != '4')
+				_putchar(x);
+		}
+		_putchar('\n');
+}
+
