26883b51df746f8d1fb25d6c5745062f785c04f2
LavoussierDev <Onilr31@gmail.com>
2023-10-29 21:34:58 -0400

    first commit

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: the size of the squares
+ */
+
+void print_square(int size)
+{
+	int square, lines;
+
+	if ( size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (lines = 1; lines <= size; lines++)
+		{
+			_putchar('#');
+
+			for (square = 2; square <= size; square++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
+	}
+}
