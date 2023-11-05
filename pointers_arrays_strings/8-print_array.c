4e01b682e7f7db7f131c91a6b385aa26d2b28776
LavoussierDev <Onilr31@gmail.com>
2023-11-04 23:12:19 -0400

    fix: betty style

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -4,7 +4,8 @@
 /**
  * print_array - prints elements of an array
  * of integers
- * @a: array @n: amount of said digit's to be printed
+ * @a: array
+ * @n: amount of said digit's to be printed
  */
 
 void print_array(int *a, int n)
