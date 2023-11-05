c14f401317b9f7800f51c3678d1d8185c525cfab
LavoussierDev <Onilr31@gmail.com>
2023-11-04 23:11:17 -0400

    runs correspondingly

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_array - prints elements of an array
@@ -8,4 +9,16 @@
 
 void print_array(int *a, int n)
 {
+	int x;
 
+	for (x = 0; x < n; x++)
+	{
+		printf("%d", a[x]);
+
+		if (x < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
