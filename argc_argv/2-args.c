bda2354bf23cd9b45bfcd8f16bb5965dddeedd73
LavoussierDev <Onilr31@gmail.com>
2023-11-05 23:53:13 -0400

    first commit

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int x = 0;
+
+	while (x < argc)
+	{
+		printf("%s\n", argv[x]);
+		x++;
+	}
+	return (0);
+}
