de965ce4ade3c236c796133e15393f62a52c4856
LavoussierDev <Onilr31@gmail.com>
2023-11-19 22:48:55 -0400

    Added Header

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns Duplicated string
+ * @str: input of characters
+ *
+ * Return: Characters
+ */
+
+char *_strdup(char *str)
+{
+
