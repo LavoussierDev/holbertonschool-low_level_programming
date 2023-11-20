3ee508fd33d9078e79ba968ec9825e2e16df99c5
LavoussierDev <Onilr31@gmail.com>
2023-11-19 23:19:37 -0400

    fix: Double inclusion

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,14 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct
+ * @name: Dog's Name
+ * @age: Dog's Age
+ * @owner: Dog's Owner
+ */
+
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
 
-typedef struct dog dog_t;
 
 void init_idog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
+#endif
