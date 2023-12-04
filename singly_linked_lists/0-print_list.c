2fb6be3679ac79b874500196d01b642822d6cfd1
LavoussierDev <Onilr31@gmail.com>
2023-12-03 23:37:53 -0400

    reformatted code with while loop

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,12 +9,22 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t x = 0;
+	size_t x;
 
-	for (; h; x++)
+	x = 0;
+	while (h != NULL)
 	{
-		printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
+		x++;
+
+		if (h->str == NULL)
+		{
+			printf("[%d] (nil)\n", 0);
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+		}
 	}
 	return (x);
 }
