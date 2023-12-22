40edf934c0ceff3b8ee94419f2c58bee0653929b
LavoussierDev <Onilr31@gmail.com>
2023-12-22 17:52:48 -0400

    added missing components when compiling

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - converts string
+ * @s: string to be converted
+ * Return: pointer to string that is converted
+ */
+
+char *leet(char *s)
+{
+	int x, y;
+	char *ch = "aeotlAEOTL";
+	char *leet = "4307143071";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (s[x] == ch[y])
+			{
+				s[x] = leet[y];
+				break;
+			}
+		}
+	}
+
+	return s;
+}
