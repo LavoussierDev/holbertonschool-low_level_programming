29b8c2e5e57b93c5805e1ad64f3a360e684794a9
LavoussierDev <Onilr31@gmail.com>
2023-12-22 21:40:16 -0400

    program working as intended

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,14 +1,39 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * _strdup - Returns Duplicated string
  * @str: input of characters
- *
- * Return: Characters
+ * Return: NULL if string NULL if not, then the chars
  */
 
 char *_strdup(char *str)
 {
+	int length = 0;
+	char *pointer, *duplicate_string;
 
+	if (!str)
+	{
+		return (NULL);
+	}
+
+	pointer = str;
+	while (*pointer++)
+	{
+		length++;
+	}
+
+	duplicate_string = malloc(length + 1);
+	if (!duplicate_string)
+	{
+		return (NULL);
+	}
+
+	pointer = duplicate_string;
+	while (*str)
+	{
+		*pointer++ = *str++;
+	}
+
+	*pointer = 0;
+	return (duplicate_string);
+}
