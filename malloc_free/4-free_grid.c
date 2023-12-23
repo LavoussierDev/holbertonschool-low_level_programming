ff9f16f2327203741c345d27c1ac3c8194864284
LavoussierDev <Onilr31@gmail.com>
2023-12-22 22:26:00 -0400

    fix: betty

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * free_grid -  frees a 2 dimensional grid previously created by alloc_grid function.
+ * free_grid -  frees array from a previous function.
  * @grid: pointer to an array of ints
  * @height: quantity of rows in the array
  */
