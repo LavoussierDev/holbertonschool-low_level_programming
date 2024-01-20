101d0420620c10fd94dd8029fa66d4269d63f95c
LavoussierDev <Onilr31@gmail.com>
2024-01-19 23:36:00 -0400

    reverted to previous code

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -10,23 +10,21 @@
  * Return: The index of matching array elements
  */
 
-int linear_search(int *arr, size_t size, int target)
-{
-	int index = 0, result = -1;
+int linear_search(int *arr, size_t size, int target) {
+    int index = 0, result = -1;
 
-	if (arr == NULL || size == 0)
-		return (-1);
+    if (arr == NULL || size == 0)
+        return -1;
 
-	while (index < (int)size && arr[index] != target)
-	{
-		printf("Value checked arr[%d] = [%d]\n", index, arr[index]);
-		index++;
-	}
+    while (index < (int)size) {
+        printf("Value checked arr[%d] = [%d]\n", index, arr[index]);
+        if (arr[index] == target) {
+            result = index;
+            break;
+        }
+        index++;
+    }
 
-	if (index < (int)size)
-	{
-		result = index;
-	}
-
-	return (result);
+    return result;
 }
+
